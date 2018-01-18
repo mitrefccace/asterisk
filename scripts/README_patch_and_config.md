0cd20c95f9efccf20f9166ddeb4e4218734db1e2
Said Masoud <SMASOUD@MITRE.ORG>
2018-01-18 10:43:26 -0500

    Merging 'asterisk-ace-direct' repo

---
+++ scripts/README_patch_and_config.md
@@ -0,0 +1,85 @@
+# Asterisk for Ace Direct
+
+### Purpose
+-----------
+This repository contains all of the necessary files and scripts required to create and track the 
+most stable version of Asterisk and PJSIP. The configuration files are the primary source of 
+modifcation to the server and should be adjusted (external media address, local address, phone number) 
+for each new server instance and then placed within __/etc/asterisk/__. Additionally, patch files have 
+been included and can be applied if desired. An installation script has been included to assist with this process.
+
+### Content
+-----------
+The __root__ directory contains the following:
+- Asterisk configuration files
+- Installation script (configs)
+
+The __./patches__ directory contains the following:
+- Asterisk patch files
+- Installation script (patches)
+
+### Installation Instructions
+----------------------------------
+1. Clone this repo to the destination. 
+2. Move into __asterisk-ace-direct/__.
+3. Execute `sudo su` 
+4. Execute one of the following commands:  
+
+|                 Command                                            | Purpose                                                                                  |
+| -------------------------------------------------------------------| -----------------------------------------------------------------------------------------|
+| `$ ./patch_and_config.sh --help`                                   | Provides instructions                                                                    |
+| `$ ./patch_and_config.sh --version 15.1.2 --patch --build`         | Applies patches to asterisk-15.1.2 source code and then rebuilds it                      |
+| `$ ./patch_and_config.sh --config --dialin 8003671294`             | Installs the configuration files using the provided phone number                         |
+| `$ ./patch_and_config.sh --patch --build --config --restart --cli` | Applies the patches, rebuilds the source, restarts Asterisk and starts the CLI           |
+
+### Configuration Files
+-----------------------------------------
+##### Variables
+* The __external_media_address__ parameter within __pjsip.conf__ requires modification. 
+This script utilizes the machine's hostname to determine the external IP address. If
+this value is null, it will query the user to input the proper domain name.
+* The __local_net__ parameter will be set using the __ifconfig__ tool. 
+* The phone number for the server must be modified within __extensions.conf__. 
+This can be supplied with the __--dialin__ flag. Otherwise, it quereis the Asterisk
+databse for the dialin number. If this value is null or has not been configured, then the 
+user will be asked to enter the phone number before proceeding. 
+* Additionally, everytime this program is run, it will check that the Asterisk DB contains 
+entries for the dialin number and both the start and end times for the call center. If any 
+of the values are null, the user will be queried. 
+
+##### Error Checking
+* The domian name is checked to make sure the __dig__ command can resolve the address. 
+If not, it returns an error. 
+* The phone number is validated by checking that it is composed of only numbers [0-9] 
+and contains only 10 total digits. If not, it returns an error.
+* The start and end times are evaluated to make sure they are in the proper ##:## format.
+
+##### File Modifications
+* These are made by executing sed commands to perform a search and replace on the files.
+* The modifications are made within a temporary directory so that the original files are 
+not altered. 
+
+##### Clean Up
+* The modified files are first copied into __/etc/asterisk/__.
+* Then the temporary directory is removed.
+* If the __--restart__ option is selected, Asterisk will be restarted. 
+* If the __--cli__ option is selected, the Command Line Interface for Asterisk will launch. 
+
+### Patch Files
+----------------------------------
+
+##### Input
+* Very little user input is required. At most, it queries the user to make sure they wish to 
+continue with a particular step. If the patch has already been applied then answer [n] for __no__ 
+so that the reverse patch is not applied. If you do not specify the Asterisk version number when 
+running, then you will be prompted for this information. 
+
+##### Process
+* The script functions by determining which files to patch by searching for the __asterisk-ace-direct__ 
+repo in the file system.
+* Once there, it reads patch files such as __foo.c.patch__ within the patch directory and searches for the 
+__foo.c__ source file within the __Asterisk-x.x.x__ directory. If the file is not found, it will alert the user. 
+* After the patches have been applied with the __patch__ command, the Asterisk source code may be recompiled 
+using the __make__ commands. Just add the __--build__ to execute these commands and build 
+the project after applying the patches.
+
---
+++ scripts/patch_and_config.sh
@@ -0,0 +1,507 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------------
+# Author   : Connor McCann
+# Project  : Ace Direct Asterisk Configuration 
+# Date     : 3 Jan 2017
+# Purpose  : To automate patches and the installation of Asterisk configuration 
+#            files on various Ace Direct servers. This script will adjust pjsip.conf
+#	     and extensions.conf for the phone number, external media address
+#	     and local net address unique to each AWS EC2 instance. 
+#----------------------------------------------------------------------------
+
+function show_instructions {
+		echo
+		echo "+--------------------------------------------------------------------------------------------------+"
+		echo "+ Run this script if:                                                                             +"
+		echo "+                      1. You would like to apply patches to Asterisk source code                 +"
+		echo "+                      2. You would like to install the configuration files to /etc/asterisk/     +"
+		echo "+                                                                                                 +"
+		echo "+                                                                                                 +"
+		echo "+ ./patch_and_config.sh [Required_Flag] [Optional_Flag]                                           +"
+		echo "+                                                                                                 +"
+		echo "+            --help    : Optional : Displays these program instructions                           +"
+		echo "+            --patch   : Required : Applies patch files to source code                            +"
+		echo "+            --config  : Required : Copies configuration files into /etc/asterisk/                +"
+		echo "+            --version : Required : Specifies which version of Asterisk to look for               +"
+		echo "+            --build   : Optional : Builds the source code                                        +"
+		echo "+            --restart : Optional : Restarts Asterisk                                             +"
+		echo "+            --cli     : Optional : Launches Asterisk CLI upon completion                         +"
+		echo "+            --dialin  : Optional : Takes phone number as next argument                           +"
+		echo "+-------------------------------------------------------------------------------------------------+"
+		echo
+}
+
+function print_message {
+        # first argument is the type of message
+        # (Error, Notify, Warning, Success)
+        colorCode="sgr0"
+        case $1 in
+                Error)
+                        colorCode=1
+                        ;;
+                Notify)
+                        colorCode=3
+                        ;;
+                Success)
+                        colorCode=2
+                        ;;
+        esac
+
+        # second argument is the message string
+        tput setaf $colorCode; printf "${1} -- "
+        tput sgr0;             printf "${2}\n"
+}
+
+function error_check_args {
+        # handle no arguments
+        if [[ $@ == "" ]]; then
+                print_message "Error" "try running './patch_and_config.sh --help' for more information  ---> exiting program"
+                exit 1
+        fi
+
+        # parse all of the input arguments
+	patch=false
+	config=false
+	build=false
+	restartArg=false
+        cliArg=false
+	nextIsNum=false
+	nextIsVersion=false
+        phoneNum=""	
+	version=""
+
+	for arg in $@
+        do
+		if [[ $nextIsNum == "true" ]]; then
+			phoneNum=$arg
+			nextIsNum=false
+			continue
+		elif [[ $nextIsVersion == "true" ]]; then
+			version=$arg
+			nextIsVersion=false
+			continue
+		fi
+		
+		case $arg in
+                        --help)
+                                show_instructions
+                                exit 0
+                                ;;
+                        --patch)
+				patch=true
+				;;
+			--config)
+				config=true
+				;;
+			--version)
+				nextIsVersion=true
+				;;
+			--build)
+				build=true
+				;;
+			--restart)
+                                restartArg=true
+                                ;;
+                        --cli)
+                                cliArg=true
+                                ;;
+			--dialin)
+				nextIsNum=true
+				;;
+                        *)
+                                print_message "Error" "unkown argument: try running './patch_and_config.sh --help' for more information  ---> exiting program"
+                                exit 1
+                esac
+        done
+	
+	# initialize the asterisk database with business hours
+	init_ast_db
+	
+	# run patchfunction 
+	if [[ $patch == "true" ]]; then
+		apply_patches $build $version	
+	fi
+
+	# run installation function
+        if [[ $config == "true" ]]; then
+		install_configs $phoneNum
+	fi
+	
+	# handle any remaingin arg commands
+	execute_args $restartArg $cliArg 
+
+}
+
+function error_check_time {
+	strLength=$(echo -n $1 | wc -m)
+	re='^[0-9]+$'
+	if [[ $1 == *":"* ]]; then
+		hour=$(echo $1 | cut -d ':' -f 1)
+		min=$(echo $1 | cut -d ':' -f 2)
+	else
+		echo "fail"
+		exit
+	fi
+	# check the length
+	if [[ $strLength != 5 ]]; then
+		echo "fail"
+	# check the hour
+	elif ! [[ $hour =~ $re ]]; then
+		echo "fail"
+	# check the minutes
+	elif ! [[ $min =~ $re ]]; then
+		echo "fail"
+	else
+		echo "pass"
+	fi	
+}
+
+function error_check_num {
+	# check that the entered value is of 10 digits in length
+	strLength=$(echo -n $1 | wc -m)
+	re='^[0-9]+$'
+	if [[ ${strLength} != 10 ]]; then
+		echo "fail"	
+	elif ! [[ $1 =~ $re ]]; then
+		echo "fail" 
+	else
+		# alert user we are accepting the number
+		echo "pass"
+	fi
+}
+
+function error_check_domain {
+	# make sure that the entered domain name will resolve
+	ip=$(dig +short	${1})
+	if [[ $ip == "" ]]; then
+		print_message "Error" "the domain you have entered does not resolve ---> Installation failed"
+		exit 1	
+	fi
+	
+	# alert the user we are accepting the domain name
+	print_message "Notify" "$1 will now be used within pjsip.conf"
+}
+
+function apply_patches {
+        # make sure asterisk-ace-direct repo is present
+        declare -a AD
+        AD=$(find / -name 'asterisk-ace-direct')
+
+        if [[ $AD == "" ]]; then
+                print_message "Error" "please clone asterisk-ace-direct repository first ---> exiting program"
+                exit 1
+        fi
+        num_AD=$(find / -name 'asterisk-ace-direct' | wc -l)
+
+        # handle multiple dirs
+        if [[ $num_AD > 1 ]]; then
+                print_message "Notify" "several directories have been found..."
+                let index=0
+
+                for dir in $AD
+                do
+                        echo "$index : $dir"
+                        let index++
+                done
+
+                # accept user input for row/dir number
+                printf "Please select which row number is the proper directory (0-indexed):\t"
+                read index
+
+                # make sure the user entered a number
+                re='^[0-9]+$'
+                while ! [[ $index =~ $re ]];
+                do
+                        printf "Incorrect value...please select which row number is the proper directory (0-indexed):\t"
+                        read index
+                done
+                AD=${AD[$index]}
+                print_message "Notify" "selecting $AD"
+        fi
+
+        # find the location of Asterisk 15.1.2
+        versionNum=$2
+	if [[ $versionNum == "" ]]; then
+		printf "Please enter an Asterisk version number:\t"
+		read versionNum
+	fi
+
+	asteriskPath=$(find / -name "asterisk-${versionNum}")
+	if [[ $asteriskPath == "" ]]; then
+		print_message "Error" "did not find that version of Asterisk anywhere ---> exiting program"
+		exit 1
+	else
+        	print_message "Notify" "found asterisk directory : ${asteriskPath}"
+	fi
+	
+        # loop over each patch in the patch directory 
+        patches=$(find ${AD} -wholename "*/${versionNum}/*.patch")
+	
+        # handle null patches
+        if [[ $patches == "" ]]; then
+                print_message "Error" "did not find any patch files ---> exiting program"
+                exit 1
+        fi
+
+        for patch in $patches
+        do
+                # determine the name of the file to be patched
+                patchFileName=$(basename $patch)
+                sourceFileName=${patchFileName:0:-6}
+
+                # find the file to be patched within Asterisk 15.1.2 
+                # this may be an array if there are several files
+                fullSourceFilePaths=$(find ${asteriskPath} -name ${sourceFileName})
+
+                # handle source file(s) not found
+                if [[ $fullSourceFilePaths == "" ]]; then
+                        print_message "Error" "did not find ${sourceFileName} ---> exiting program"
+                        exit 1
+                fi
+
+                for fullSourceFilePath in $fullSourceFilePaths
+                do
+                        sourceFilePath=$(dirname ${fullSourceFilePath})
+                        print_message "Notify" "found ${sourceFileName} in ${sourceFilePath}"
+
+                        # apply the patch
+                        originalFile=$fullSourceFilePath
+                        patch $originalFile $patch
+                done
+        done
+
+        # alert user of patch completion 
+        print_message "Success" "patches have been applied"
+
+        # rebuild source
+        if [[ $1 == "true" ]]; then
+                printf "Continue building Asterisk from source (yes/no)? : "
+                read userResp
+
+                if [[ $userResp == "yes" ]] || [[ $userResp == "y" ]]; then
+                        # configure 
+                        print_message "Notify" "moving to ${asteriskPath}"
+                        cd $asteriskPath
+                        # make
+                        source /etc/environment
+			make && make install
+			ldconfig
+			# move back into the AD repo
+			print_message "Notify" "moving back into ${AD}"
+			cd $AD
+                else
+                        # handle no 
+                        print_message "Notify" "aborting the build process ---> Pacthes applied but source not built"
+                fi
+        fi
+}
+
+function install_configs {
+	hostName=$(hostname)
+	if [[ $hostName == "" ]]; then 
+		# get the hostname from the user
+		printf "Please enter the domain name for this server:\t"
+		read hostName
+	fi
+	error_check_domain $hostName
+			
+	# the new information
+	newNum=$1
+	if [[ $newNum  == "" ]]; then
+		# try to query asterisk db for call center phone number
+		newNum=$(execute_asterisk_command "database get GLOBAL DIALIN" | cut -d ' ' -f 2)
+	fi
+	output=$(error_check_num $newNum)
+	if [[ $output == "fail" ]]; then
+		print_message "Error" "the number you have entered is improper format ---> Installation failed"
+		exit 1
+	elif [[ $output == "pass" ]]; then
+		print_message "Notify" "${newNum} will now be used within extenstions.conf"
+	fi
+
+	newIP=$(dig +short ${hostName})
+	newLocalNet=$(ifconfig | grep inet -m 1 | cut -d ' ' -f 10)
+
+	if [[ ${newIP} != "" ]] && [[ ${newLocalNet} != "" ]]; then
+		# status for user info
+		configStatus=true
+	
+		# create an array to hold exit codes
+		declare -a exitCodes
+		let index=0
+
+		# modify the files with vim
+		mkdir temp
+		cp ./configs/pjsip.conf ./configs/extensions.conf ./temp
+		
+		# change the external media address
+		sed -i "s/external_media_address=*.*.*.*/external_media_address=${newIP}/g" ./temp/pjsip.conf && \
+		sed -i "s/media_address=*.*.*.*/media_address=${newIP}/g" ./temp/pjsip.conf && \
+		sed -i "s/external_signaling_address=*.*.*.*/external_signaling_address=${newIP}/g" ./temp/pjsip.conf && \
+		sed -i "s/bind_rtp_to_media_address=*.*.*.*/bind_rtp_to_media_address=${newIP}/g" ./temp/pjsip.conf
+		
+		exitCodes[$index]=$?
+		let index++
+	
+		# change the local net address
+		sed -i "s/local_net=*.*.*.*/local_net=${newLocalNet}/g" ./temp/pjsip.conf
+		
+		exitCodes[$index]=$?
+		let index++
+
+		# alert user if mods to pjsip.conf were successfull
+		problem=false
+		for code in ${exitCodes[@]}
+		do
+			if [[ $code == 1 ]]; then
+				problem=true
+				configStatus=false
+				break
+			fi
+		done
+
+		if [[ !problem ]]; then
+			print_message "Notify" "modified pjsip.conf successfully"
+		else
+			print_message "Error" "there was a problem modifying pjsip.conf"
+			configStatus=false
+		fi
+
+		# change the phone number associated with Asterisk for inbound calls from provider devices
+		sed -i "s/[0-9]\{10\}/${newNum}/g" ./temp/extensions.conf 
+		
+		# alert user if mods to extensions.conf were successfull
+		if [[ $? == 0 ]]; then
+			print_message "Notify" "modified extensions.conf successfully"
+		else
+			print_message "Error" "there was a problem modifying extensions.conf"
+			configStatus=false
+		fi
+	else
+		# handle the error
+		print_message "Error" "could not resolve hostname ---> Installation failed"
+		exit 1
+	fi
+
+	# replace the asterisk config files
+	cp ./temp/pjsip.conf /etc/asterisk/
+	cp ./temp/extensions.conf /etc/asterisk/
+
+	if [[ $? == 0 ]]; then
+		print_message "Notify" "copied ./configs/pjsip.conf ---> /etc/asterisk/"
+		print_message "Notify" "copied ./configs/extensions.conf ---> /etc/asterisk/"
+	else
+		configStatus=false
+	fi
+
+	# replace the rest of the configuration files
+	configFiles=$(find ./ -name '*.conf' -and -not -name 'pjsip.*' -and -not -name 'extensions.*')
+	for file in $configFiles
+	do
+		cp $file /etc/asterisk/
+		if [[ $? == 0 ]]; then
+			print_message "Notify" "copied ${file} ---> /etc/asterisk/"
+		else
+			configStatus=false
+		fi
+	done
+	
+	# clean up
+	sudo rm -r ./temp
+
+	# setup complete
+	if [[ $configStatus == "true" ]]; then
+		print_message "Success"  "Configuration complete"
+	else
+		print_message "Error" "failed to install configuration files"
+	fi
+}	
+
+function execute_args {
+	# restart asterisk so the changes are in effect
+	if [[ $1 == "true" ]]; then
+		service asterisk restart
+	fi
+
+	# start the CLI interface
+	if [[ $2 == "true" ]]; then
+		sleep 2
+		asterisk -rvvvvvvvc
+	fi
+}
+
+function execute_asterisk_command {
+	output=$(sudo asterisk -rx "${1}")	
+	echo $output
+}
+
+function init_ast_db {
+	# check for the dialin number
+	output=$(asterisk -rx "database get GLOBAL DIALIN" | cut -d ' ' -f 2)
+	if [[ $output == "entry" ]]; then
+		printf "Please provide the call center dialin number: "
+		read dialin
+		rez=$(error_check_num $dialin)
+		while [ $rez == "fail" ]
+		do
+			printf "You entered an improper dialin number. Please try again: "
+			read dialin
+			rez=$(error_check_num $dialin)
+		done
+		rez=$(asterisk -rx "database put GLOBAL DIALIN ${dialin}")
+		if [[ $rez == "Updated database successfully" ]]; then
+			print_message "Success" "Asterisk dialin number <${dialin}> has been added to the Asterisk database"
+		else
+			print_message "Error" "could not add dialin number to the Asterisk database"
+		fi 
+	elif [[ $output == "" ]]; then
+		print_message "Error" "asterisk service is down ---> Installation failed."
+		exit 1
+	fi
+	
+	# check for business hours start time
+	output=$(asterisk -rx "database get BUSINESS_HOURS START" | cut -d ' ' -f 2)
+	if [[ $output == "entry" ]]; then
+		printf "Please provide the call center start time: "
+		read start_time
+		rez=$(error_check_time $start_time)
+		while [ $rez == "fail" ]
+		do
+			printf "You entered an improper start time. Please try again: "
+			read start_time
+			rez=$(error_check_time $start_time)
+		done
+		rez=$(asterisk -rx "database put BUSINESS_HOURS START ${start_time}")
+		if [[ $rez == "Updated database successfully" ]]; then
+			print_message "Success" "business hours start time <${start_time}> has been added to the Asterisk database"
+		else
+			print_message "Error" "could not add business hours start time to the Asterisk database"
+		fi 
+	fi 
+	# check for business hours end time
+	output=$(asterisk -rx "database get BUSINESS_HOURS END" | cut -d ' ' -f 2)
+	if [[ $output == "entry" ]]; then
+		printf "Please provide the call center end time: "
+		read end_time
+		rez=$(error_check_time $end_time)
+		while [ $rez == "fail" ]
+		do
+			printf "You entered an improper end time. Please try again: "
+			read end_time
+			rez=$(error_check_time $end_time)
+		done
+		rez=$(asterisk -rx "database put BUSINESS_HOURS END ${start_time}")
+		if [[ $rez == "Updated database successfully" ]]; then
+			print_message "Success" "business hours end time <${end_time}>  has been added to the Asterisk database"
+		else
+			print_message "Error" "could not add business hours end time to the Asterisk database"
+		fi 
+	fi
+}
+
+# execute the main function 
+error_check_args $@
+
+# EOF
+
