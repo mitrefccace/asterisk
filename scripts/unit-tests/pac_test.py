41fde948b5624f4be952212926ea0b32593ecc84
Connor <cmccann@mitre.org>
2018-02-07 17:31:31 +0000

    adding test for PAC script (wip)'

---
+++ scripts/unit-tests/pac_test.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+# Project : ACE Direct Automated Installation 
+# Author  : Connor McCann
+# Date    : 07 Feb 2018
+# Purpose : to provide a unit test for the PAC script which 
+#	    is responsible for patching Asterisk source code,
+#           installing configuration files, and setting up
+#           the Asterisk internal database.
+
+import unittest
+import subprocess as sub
+import sys
+import time
+import os
+
+
+class CornerCases(unittest.TestCase):
+		
+	@classmethod
+	def setUpClass(cls):
+		os.chdir('../')
+	
+	def test_ast_service_down(self):
+       		# stop asterisk service
+		com = OsCommand('sudo service asterisk stop')
+		rez = com.execute(1)
+       	
+       		# check the output of PAC
+		com = OsCommand('sudo ./patch_and_config.sh --config')
+		rez = com.execute(0)
+		correct = '\x1b[31mError -- \x1b(B\x1b[mAsterisk service is unreachable ---> Installation canceled\n'
+		self.assertEqual(rez, correct)
+       	
+		# start the asterisk service back up
+		com = OsCommand('sudo service asterisk start')
+		rez = com.execute(1)
+       
+	def test_bad_input(self):
+       		# check for unrecognized command line args
+		com = OsCommand('sudo ./patch_and_config.sh --config patch ---no-db')
+		rez = com.execute(0)
+		correct = "\x1b[31mError -- \x1b(B\x1b[munkown argument: try running './patch_and_config.sh --help' for more information  ---> exiting program\n"
+		self.assertEqual(rez, correct)
+       
+	def test_config_install(self):
+       		# check the entire configuration process for successs
+		com = OsCommand('sudo ./patch_and_config.sh --config')
+		rez = com.execute(0)
+		lines = rez.split('\n')
+		status = lines.pop()
+		while status is '':
+			status = lines.pop()
+		correct = '\x1b[32mSuccess -- \x1b(B\x1b[mConfiguration complete'
+		self.assertEqual(status, correct)
+	
+	def test_no_dialin_no_db(self):
+		# get the old number 
+		com = AstCommand('database get GLOBAL DIALIN')
+		rez = com.execute(0)
+		dialin = rez.split(':')[1].strip()
+
+		# check that the db function is triggered
+		com = AstCommand('database del GLOBAL DIALIN')
+		rez = com.execute(0)
+		correct = 'Database entry removed.\n'
+		self.assertEqual(rez, correct) 
+	
+		# check the behavior of PAC
+		com = OsCommand('sudo ./patch_and_config.sh --config --no-db')
+		rez = com.execute(0)
+		lines = rez.split('\n')
+		status = lines.pop()
+		while status is '':
+			status = lines.pop()
+		correct = '\x1b[31mError -- \x1b(B\x1b[mthe Asterisk database does not contain a dialin number ---> Installation Canceled'
+		self.assertEqual(status, correct)
+		
+		# return the value	
+		com = AstCommand('database put GLOBAL DIALIN {}'.format(dialin))
+		rez = com.execute(0)
+
+	@classmethod
+	def tearDownClass(cls):
+		os.chdir('./unit-tests')
+
+class OsCommand:
+	
+	def __init__(self, cs):
+		self.command_string = cs
+		self.command_list = cs.split()
+	
+	def execute(self, sleep_time):
+		ps = sub.Popen(self.command_list, stdout=sub.PIPE)
+		(output, err) = ps.communicate()
+		if sleep_time:
+			time.sleep(sleep_time)
+		if not err:
+			return output
+
+class AstCommand:
+	
+	def __init__(self, cs):
+		self.command_string = cs
+	
+	def execute(self, sleep_time):
+		command = ['sudo', 'asterisk', '-rx']
+		command.append(self.command_string)
+		ps = sub.Popen(command, stdout=sub.PIPE)
+		(output, err) = ps.communicate()
+		if sleep_time:
+			time.sleep(sleep_time)
+		if not err:
+			return output
+
+
+if __name__ == '__main__':
+	sys.exit(unittest.main())
+
