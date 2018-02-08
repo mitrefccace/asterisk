48eaeaff2d77970bbb260a1f8d7c7ec6c482ec04
Connor <cmccann@mitre.org>
2018-02-08 14:53:52 +0000

    minor updates to unit test

--- scripts/unit-tests/pac_test.py
+++ scripts/unit-tests/pac_test.py
@@ -8,8 +8,9 @@
 #           installing configuration files, and setting up
 #           the Asterisk internal database.
 
-import unittest
 import subprocess as sub
+import unittest
+import abc
 import sys
 import time
 import os
@@ -84,36 +85,36 @@ class CornerCases(unittest.TestCase):
 	def tearDownClass(cls):
 		os.chdir('./unit-tests')
 
-class OsCommand:
-	
-	def __init__(self, cs):
-		self.command_string = cs
-		self.command_list = cs.split()
-	
+class BaseCommand(object):
+	__metaclass__ = abc.ABCMeta
+
+	@abc.abstractmethod
 	def execute(self, sleep_time):
 		ps = sub.Popen(self.command_list, stdout=sub.PIPE)
 		(output, err) = ps.communicate()
 		if sleep_time:
 			time.sleep(sleep_time)
 		if not err:
-			return output
+			return output			
 
-class AstCommand:
+class OsCommand(BaseCommand):
 	
 	def __init__(self, cs):
-		self.command_string = cs
-	
+		self.command_list = cs.split()
+
 	def execute(self, sleep_time):
-		command = ['sudo', 'asterisk', '-rx']
-		command.append(self.command_string)
-		ps = sub.Popen(command, stdout=sub.PIPE)
-		(output, err) = ps.communicate()
-		if sleep_time:
-			time.sleep(sleep_time)
-		if not err:
-			return output
+		return super(OsCommand, self).execute(sleep_time)
 
+class AstCommand(BaseCommand):
+	
+	def __init__(self, cs):
+		self.command_list = ['sudo', 'asterisk', '-rx']
+		self.command_list.append(cs)
 
+	def execute(self, sleep_time):
+		return super(AstCommand, self).execute(sleep_time)		
+
+	
 if __name__ == '__main__':
 	sys.exit(unittest.main())
 
