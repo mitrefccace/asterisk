fceb9dd6a2bd999f8fe76743753e27edcd46284c
Cloud User <centos@vasip7.task3acrdemo.com>
2018-03-16 15:47:55 +0000

    needed to add a comment in the code

--- scripts/update_asterisk.sh
+++ scripts/update_asterisk.sh
@@ -466,6 +466,10 @@ function install_configs {
 				# this statement will evaluate to true
 				if [ $( dirname $file) != "." ]; then
 					sed -i 's|'$tag'|'$value'|g' "$file"
+					# ***********************************************************
+					# this is a work-around to handle the '&' in the cdr password 
+					# if we change the pw then this logic should be adjusted 
+					# ***********************************************************
 					if [ $tag == "<cdr_pass>" ]; then
 						sed -i 's|'$tag'|'\&'|g' "$file"
 					fi
